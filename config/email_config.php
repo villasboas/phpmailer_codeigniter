8864e4d3eeae3c9927f517b2fb8559b5013b80e6
Gustavo Vilas Bôas <gu.boas13@gmail.com>
2016-08-05 10:11:25 -0300

    Adiciona o arquivo de configurações

---
+++ config/email_config.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * Configuração do PHP Mailer
+ */
+$config["host"]      = "";
+$config["port"]      = 587;
+$config["from"]      = "";
+$config["password"]  = "";
+$config["from_name"] = "";  
+?>
